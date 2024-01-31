325cd2ea31598f9361cc27a09f287ef3233a7629
martyna <fyrodems@gmail.com>
2024-01-31 15:51:01 +0100

    MainBannerSlider refactor

--- src/components/homepage/MainBannerSlider/MainBannerSlider.tsx
+++ src/components/homepage/MainBannerSlider/MainBannerSlider.tsx
@@ -1,152 +1,122 @@
-import { useRef, useState, MouseEvent } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from './MainBannerSlider.module.scss';
 import classNames from 'classnames';
 
-type Color = 'black' | 'brown' | 'red' | 'string';
+type Color = 'black' | 'brown' | 'red';
+
+interface SliderProps {
+  color: Color;
+  transitioning: boolean;
+  selectedButton: Color;
+  handlePaginationButtonClick: (color: Color) => void;
+  handleFancyPaginationButtonClick: (color: Color) => void;
+}
+
+const SliderImage: React.FC<{
+  currentColor: Color;
+  transitioning: boolean;
+}> = ({ currentColor, transitioning }) => (
+  <div className={styles.imageContainer}>
+    <Image
+      className={classNames(styles.image, {
+        [styles.transitioning]: transitioning,
+      })}
+      width={150}
+      height={150}
+      alt="fireplace"
+      src={`/assets/mainBanner/${currentColor}.webp`}
+    />
+  </div>
+);
+
+const PaginationButton: React.FC<{
+  color: Color;
+  selected: boolean;
+  onClick: () => void;
+}> = ({ color, selected, onClick }) => (
+  <div
+    className={classNames(
+      styles.paginationButton,
+      styles[`paginationButton__${color}`],
+      {
+        [styles.selectedButton]: selected,
+      }
+    )}
+    onClick={onClick}
+    style={
+      selected ? { transform: 'scale(1.3)', border: '4px solid' } : undefined
+    }
+  />
+);
+
+const FancyPaginationButton: React.FC<{
+  color: Color;
+  selected: boolean;
+  onClick: () => void;
+}> = ({ color, selected, onClick }) => (
+  <div
+    className={classNames(
+      styles.fancyPaginationButton,
+      styles[`fancyPaginationButton__${color}`],
+      {
+        [styles.selectedButton]: selected,
+      }
+    )}
+    onClick={onClick}
+  >
+    <Image
+      className={classNames(styles.fancyImage)}
+      width={150}
+      height={150}
+      alt="fireplace"
+      src={`/assets/mainBanner/${color}.webp`}
+    />
+  </div>
+);
 
 export const MainBannerSlider: React.FC = () => {
   const [currentColor, setCurrentColor] = useState<Color>('black');
   const [transitioning, setTransitioning] = useState<boolean>(false);
   const [selectedButton, setSelectedButton] = useState<Color>('black');
-  const prevSelectedButtonRef = useRef<HTMLDivElement | null>(null);
 
-  const handlePaginationButtonClick = (
-    color: Color,
-    buttonRef: HTMLDivElement
-  ) => {
+  const handlePaginationButtonClick = (color: Color) => {
     setTransitioning(true);
     setTimeout(() => {
       setCurrentColor(color);
       setTransitioning(false);
     }, 500);
 
-    // Zmiana bordera dla aktualnie wybranego przycisku
-    if (selectedButton !== color) {
-      setSelectedButton(color);
-      buttonRef.style.borderWidth = '4px';
-      buttonRef.style.transform = 'scale(1.2)';
-
-      // Zmiana bordera dla poprzednio wybranego przycisku
-      if (prevSelectedButtonRef.current) {
-        prevSelectedButtonRef.current.style.borderWidth = '2px';
-        prevSelectedButtonRef.current.style.transform = 'scale(1)';
-      }
-
-      prevSelectedButtonRef.current = buttonRef;
-    }
-  };
-
-  const handleFancyPaginationButtonClick = (color: Color) => {
-    setTransitioning(true);
-    setTimeout(() => {
-      setCurrentColor(color);
-      setTransitioning(false);
-    }, 500);
+    setSelectedButton(color);
   };
 
   return (
     <div className={styles.mainBannerSlider}>
       <div className={styles.sliderWrapper}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={classNames(styles.image, {
-              [styles.transitioning]: transitioning,
-            })}
-            width={150}
-            height={150}
-            alt="fireplace"
-            src={`/assets/mainBanner/${currentColor}.webp`}
-          />
-        </div>
+        <SliderImage
+          currentColor={currentColor}
+          transitioning={transitioning}
+        />
         <div className={styles.paginationWrapper}>
-          <div
-            className={classNames(
-              styles.paginationButton,
-              styles.paginationButton__red,
-              { [styles.selectedButton]: selectedButton === 'red' }
-            )}
-            onClick={(e: MouseEvent<HTMLDivElement>) =>
-              handlePaginationButtonClick('red', e.currentTarget)
-            }
-          ></div>
-
-          <div
-            className={classNames(
-              styles.paginationButton,
-              styles.paginationButton__black,
-              { [styles.selectedButton]: selectedButton === 'black' }
-            )}
-            onClick={(e: MouseEvent<HTMLDivElement>) =>
-              handlePaginationButtonClick('black', e.currentTarget)
-            }
-          ></div>
-          <div
-            className={classNames(
-              styles.paginationButton,
-              styles.paginationButton__brown,
-              { [styles.selectedButton]: selectedButton === 'brown' }
-            )}
-            onClick={(e: MouseEvent<HTMLDivElement>) =>
-              handlePaginationButtonClick('brown', e.currentTarget)
-            }
-          ></div>
+          {['red', 'black', 'brown'].map((color) => (
+            <PaginationButton
+              key={color}
+              color={color as Color}
+              selected={selectedButton === color}
+              onClick={() => handlePaginationButtonClick(color as Color)}
+            />
+          ))}
         </div>
       </div>
       <div className={styles.fancyPagination}>
-        <div
-          className={classNames(
-            styles.fancyPaginationButton,
-            styles.fancyPaginationButton__red
-          )}
-          onClick={(e: MouseEvent<HTMLDivElement>) =>
-            handleFancyPaginationButtonClick('red')
-          }
-        >
-          <Image
-            className={classNames(styles.fancyImage)}
-            width={150}
-            height={150}
-            alt="fireplace"
-            src={`/assets/mainBanner/red.webp`}
+        {['red', 'black', 'brown'].map((color) => (
+          <FancyPaginationButton
+            key={color}
+            color={color as Color}
+            selected={selectedButton === color}
+            onClick={() => handlePaginationButtonClick(color as Color)}
           />
-        </div>
-
-        <div
-          className={classNames(
-            styles.fancyPaginationButton,
-            styles.fancyPaginationButton__black
-          )}
-          onClick={(e: MouseEvent<HTMLDivElement>) =>
-            handleFancyPaginationButtonClick('black')
-          }
-        >
-          <Image
-            className={classNames(styles.fancyImage)}
-            width={150}
-            height={150}
-            alt="fireplace"
-            src={`/assets/mainBanner/black.webp`}
-          />
-        </div>
-
-        <div
-          className={classNames(
-            styles.fancyPaginationButton,
-            styles.fancyPaginationButton__brown
-          )}
-          onClick={(e: MouseEvent<HTMLDivElement>) =>
-            handleFancyPaginationButtonClick('brown')
-          }
-        >
-          <Image
-            className={classNames(styles.fancyImage)}
-            width={150}
-            height={150}
-            alt="fireplace"
-            src={`/assets/mainBanner/brown.webp`}
-          />
-        </div>
+        ))}
       </div>
     </div>
   );
