49d267fc6fcaa7a4a923a7c2dfd3eff0c22ff05c
martyna <fyrodems@gmail.com>
2024-02-02 21:40:43 +0100

    attributes section

--- src/components/about/Attributes/Attributes.tsx
+++ src/components/about/Attributes/Attributes.tsx
@@ -1,9 +1,48 @@
+import Image from 'next/image';
 import styles from './Attributes.module.scss';
 
 export const Attributes = () => {
   return (
     <>
       <h2 className={styles.sectionTitle}>Dlaczego my?</h2>
+      <div className={styles.attributesWrapper}>
+        <div className={styles.attribute}>
+          <Image
+            src="/assets/about/attributes1.svg"
+            width={80}
+            height={80}
+            alt=""
+          />
+          <span>Dokładność</span>
+        </div>
+        <div className={styles.attribute}>
+          <Image
+            src="/assets/about/attributes2.svg"
+            width={80}
+            height={80}
+            alt=""
+          />
+          <span>Dokładność</span>
+        </div>
+        <div className={styles.attribute}>
+          <Image
+            src="/assets/about/attributes3.svg"
+            width={80}
+            height={80}
+            alt=""
+          />
+          <span>Dokładność</span>
+        </div>
+        <div className={styles.attribute}>
+          <Image
+            src="/assets/about/attributes4.svg"
+            width={80}
+            height={80}
+            alt=""
+          />
+          <span>Dokładność</span>
+        </div>
+      </div>
     </>
   );
 };
