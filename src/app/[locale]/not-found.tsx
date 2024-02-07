31bc4b3ae28bccf91a0b12d6145d73c54b089c93
martyna <fyrodems@gmail.com>
2024-02-07 14:17:00 +0100

    styled not found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,3 +1,27 @@
+import { Button } from '@/components/common/Button/Button';
+import { PageTitle } from '@/components/common/PageTitle/PageTitle';
+import styles from '../styles/NotFound.module.scss';
+
 export default function NotFoundPage() {
-  return <h1>404</h1>;
+  return (
+    <div className={styles.notFoundPage}>
+      <PageTitle
+        pageTitle={'404'}
+        supportingText={'Nie znaleziono strony'}
+        mainColor={'#1B1F2D'}
+        gradient="to-right"
+      />
+      <div className={styles.content}>
+        <h2>Ups!</h2>
+        <p>
+          Wygląda na to, że zgubiłeś się w płomieniach i dymie. <br /> Strona,
+          której szukasz, niestety, jest jak dym <br /> z kominka - gdzieś się
+          ulatuje!
+        </p>
+        <div className={styles.buttonWrapper}>
+          <Button to="/" content="Wróć na stronę główną" />
+        </div>
+      </div>
+    </div>
+  );
 }
