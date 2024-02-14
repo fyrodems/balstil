b9081b17243a820cc7404650a30299192dae8923
martyna <fyrodems@gmail.com>
2024-02-14 12:35:48 +0100

    add useWindowDimensions hook

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -2,18 +2,20 @@
 
 import { useState, useEffect, useRef } from 'react';
 import styles from './Navbar.module.scss';
-import { isMobileWidth } from '@/utils/functions';
+// import { isMobileWidth } from '@/utils/functions';
 import { Hamburger } from '../Hamburger/Hamburger';
 import { Link } from '@/navigation';
 import Image from 'next/image';
 import { useLocale } from 'next-intl';
 import { usePathname } from 'next/navigation';
+import useWindowDimensions from '@/utils/useWindowDimensions';
 
 export const Navbar: React.FC = () => {
   const [isDropdownOpen, setDropdownOpen] = useState<boolean>(false);
   const dropdownRef = useRef<HTMLDivElement | null>(null);
   const locale = useLocale() as 'en' | 'pl' | undefined;
   const pathname = usePathname();
+  const { width, height } = useWindowDimensions();
 
   const handleOutsideClick = (event: MouseEvent) => {
     if (
@@ -45,6 +47,87 @@ export const Navbar: React.FC = () => {
         <div className={styles.healper}></div>
       </div>
       {isDropdownOpen && (
+        <nav className={styles.mobileNavigation}>
+          <div className={styles.innerHamburger}>
+            <Hamburger
+              setDropdownOpen={setDropdownOpen}
+              isDropdownOpen={isDropdownOpen}
+            />
+          </div>
+
+          <div className={styles.upperContainer}>
+            <Link href="/">
+              <Image width={160} height={40} src={'/assets/logo.png'} alt="" />
+            </Link>
+
+            <div className={styles.menuLinks}>
+              <div>
+                <h2>Lorem ipsum</h2>
+                <ul>
+                  <li>
+                    <Link locale={locale} href={`/about`}>
+                      O nas
+                    </Link>
+                  </li>
+                  <li>
+                    <Link locale={locale} href={`/product`}>
+                      Przykładowy produkt
+                    </Link>
+                  </li>
+                  <li>
+                    <Link locale={locale} href={`/404`}>
+                      Strona 404
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2>Mój profil</h2>
+                <ul>
+                  <li>
+                    <Link locale={locale} href={`#`}>
+                      Zamówienia
+                    </Link>
+                  </li>
+                  <li>
+                    <Link locale={locale} href={`#`}>
+                      Ustawienia
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <ul>
+              <li>
+                <Link locale={locale} href={`#`}>
+                  Zamówienia
+                </Link>
+              </li>
+              <li>
+                <Link locale={locale} href={`#`}>
+                  Ustawienia
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
+    </>
+  );
+
+  const desktopNavBar = (
+    <div className={styles.headerContainer}>
+      <Hamburger
+        setDropdownOpen={setDropdownOpen}
+        isDropdownOpen={isDropdownOpen}
+      />
+      <Link href="/">
+        <Image width={180} height={40} src={'/assets/logo.png'} alt="" />
+      </Link>
+      {isDropdownOpen && (
         <nav className={`${styles.mobileNavigation} ${styles.headerBg}`}>
           <ul>
             <li>
@@ -65,14 +148,6 @@ export const Navbar: React.FC = () => {
           </ul>
         </nav>
       )}
-    </>
-  );
-
-  const desktopNavBar = (
-    <div className={styles.headerContainer}>
-      <Link href="/">
-        <Image width={180} height={40} src={'/assets/logo.png'} alt="" />
-      </Link>
       <nav className={`${styles.desktopNavigation}`}>
         <ul>
           <li
@@ -115,7 +190,7 @@ export const Navbar: React.FC = () => {
   return (
     <>
       <div data-name="dropdownRef" ref={dropdownRef}>
-        {isMobileWidth() ? mobileNavBar : desktopNavBar}
+        {width && width < 992 ? mobileNavBar : desktopNavBar}
       </div>
     </>
   );
