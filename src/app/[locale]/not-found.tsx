e1d032b1eb4f11d6f720e342b88b5f72b34afc3e
martyna <fyrodems@gmail.com>
2024-02-22 14:46:59 +0100

    add useTranslations to not-found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,25 +1,24 @@
 import { Button } from '@/components/common/Button/Button';
 import { PageTitle } from '@/components/common/PageTitle/PageTitle';
 import styles from '../styles/NotFound.module.scss';
+import { useTranslations } from 'next-intl';
 
 export default function NotFoundPage() {
+  const t = useTranslations('404');
+
   return (
     <div className={styles.notFoundPage}>
       <PageTitle
         pageTitle={'404'}
-        supportingText={'Nie znaleziono strony'}
+        supportingText={t('supportingText')}
         mainColor={'#1B1F2D'}
         gradient="to-right"
       />
       <div className={styles.content}>
         <h2>Ups!</h2>
-        <p>
-          Wygląda na to, że zgubiłeś się w płomieniach i dymie.
-          <br />
-          Strona, której szukasz, niestety gdzieś się ulotniła!
-        </p>
+        <p>{t('text')}</p>
         <div className={styles.buttonWrapper}>
-          <Button to="/" content="Wróć na stronę główną" />
+          <Button to="/" content={t('buttonContent')} />
         </div>
       </div>
     </div>
