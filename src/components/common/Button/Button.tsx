e8ba472d2885b215dff728fd7582c3801ec2a568
fyrodems <fyrodems@gmail.com>
2024-02-23 11:07:34 +0100

    fix link button locale

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
 // import { Link } from '@/navigation';
-import classNames from 'classnames';
-import Link from 'next/link';
-import { FC, useState } from 'react';
+import classNames from "classnames";
+import { useLocale } from "next-intl";
+import Link from "next/link";
+import { FC, useState } from "react";
 
 interface ButtonProps {
-  type?: 'primary' | 'secondary' | 'tertiary';
+  type?: "primary" | "secondary" | "tertiary";
   content?: string | React.ReactNode;
   children?: React.ReactNode;
   showArrow?: boolean;
@@ -15,25 +16,26 @@ interface ButtonProps {
 }
 
 export const Button: FC<ButtonProps> = ({
-  type = 'primary',
+  type = "primary",
   content,
   children,
   showArrow = true,
   className,
   to,
 }) => {
+  const locale = useLocale() as "en" | "pl" | undefined;
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <Link href={to}>
+    <Link locale={locale} href={to}>
       <button
         className={classNames(
           className,
-          type === 'primary'
-            ? 'bg-main-color text-white'
-            : 'bg-button-bg text-black',
-          'font-medium flex center rounded-[50px] p-2 px-5',
-          'relative overflow-hidden transition-transform duration-300'
+          type === "primary"
+            ? "bg-main-color text-white"
+            : "bg-button-bg text-black",
+          "font-medium flex center rounded-[50px] p-2 px-5",
+          "relative overflow-hidden transition-transform duration-300"
         )}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
@@ -45,16 +47,16 @@ export const Button: FC<ButtonProps> = ({
             <div className="w-7 h-7 relative ml-2">
               <img
                 src={
-                  type === 'primary'
-                    ? '/assets/common/buttonArrow.svg'
-                    : type === 'secondary'
-                    ? '/assets/common/buttonArrowBlack.svg'
-                    : '/assets/common/buttonArrowGold.svg'
+                  type === "primary"
+                    ? "/assets/common/buttonArrow.svg"
+                    : type === "secondary"
+                    ? "/assets/common/buttonArrowBlack.svg"
+                    : "/assets/common/buttonArrowGold.svg"
                 }
                 alt=""
                 className={classNames(
-                  'w-full h-full absolute transform transition-transform duration-300 ',
-                  { 'translate-x-[4px] translate-y-[-4px]': isHovered }
+                  "w-full h-full absolute transform transition-transform duration-300 ",
+                  { "translate-x-[4px] translate-y-[-4px]": isHovered }
                 )}
               />
             </div>
