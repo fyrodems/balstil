faddeea1d1ae72402c1f00acfc3e4accebef85f8
fyrodems <fyrodems@gmail.com>
2024-02-23 13:16:11 +0100

    add new newsletter

--- src/components/homepage/Newsletter/Newsletter.tsx
+++ src/components/homepage/Newsletter/Newsletter.tsx
@@ -1,31 +1,42 @@
-import { Button } from '@/components/common/Button/Button';
-import styles from './Newsletter.module.scss';
+import { Button } from "@/components/common/Button/Button";
+import styles from "./Newsletter.module.scss";
 
 export const Newsletter = () => {
   return (
     <div className={styles.newsletterWrapper}>
-      <h2>Newsletter</h2>
-      <div className={styles.secondaryHeading}>Bądź na bieżąco</div>
-      <form className={styles.formWrapper}>
-        <div className={styles.inputsWrapper}>
-          <div>
+      <div className={styles.description}>
+        <h2>
+          Zapisz się do
+          <br />
+          newslettera BÅLSTIL
+        </h2>
+        <p>
+          Dołącz do naszego newslettera i otrzymuj ekskluzywne|aktualizacje,
+          atrakcyjne oferty oraz cenne informacje bezpośrednio na swoją skrzynkę
+          mailową!
+        </p>
+      </div>
+
+      <form className={styles.form}>
+        <div className={styles.inputWrapper}>
+          <div className={styles.input}>
             <label htmlFor="email">E-mail</label>
             <input type="email" placeholder="E-mail" id="email" />
           </div>
-          <div>
-            <label htmlFor="tel">Numer telefonu</label>
-            <input type="tel" placeholder="Numer telefonu" id="tel" />
-          </div>
+          <Button
+            to="#"
+            type="primary"
+            content="Zapisz się"
+            className={styles.formButton}
+          />
         </div>
 
-        <div className={styles.agreementsWrapper}></div>
-
-        <Button
-          to="#"
-          type="secondary"
-          content="Zapisz się"
-          className={styles.formButton}
-        />
+        <div className={styles.agreementsWrapper}>
+          Zapisując się do Newslettera wyrażam zgodę na otrzymywanie drogą
+          elektroniczną na wskazany przeze mnie adres email informacji
+          handlowej. Zapoznałem się z Regulaminem Sklepu internetowego oraz
+          akceptuję jego postanowienia.
+        </div>
       </form>
     </div>
   );
