3352cce5ef37fe61559fb38e2b5e0ee1caa5be9d
fyrodems <fyrodems@gmail.com>
2024-02-26 15:12:17 +0100

    delete footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,27 +1,11 @@
-'use client';
+"use client";
 
-import { useLocale, useTranslations } from 'next-intl';
-import styles from './Footer.module.scss';
-import Logotype from './Logotype/Logotype';
-import ContactInfo from './ContactInfo/ContactInfo';
-import Sitemap from './Sitemap/Sitemap';
-import BottomLinks from './BottomLinks/BottomLinks';
+import { useLocale, useTranslations } from "next-intl";
+import styles from "./Footer.module.scss";
 
 export const Footer = () => {
   // const t = useTranslations("layout.footer");
   const locale = useLocale();
 
-  return (
-    <footer className={styles.footerWrapper}>
-      <div>
-        <Logotype />
-        <div className={styles.mainContent}>
-          <ContactInfo />
-          <div className={styles.decoration} />
-          <Sitemap />
-        </div>
-        <BottomLinks />
-      </div>
-    </footer>
-  );
+  return <footer className={styles.footerWrapper}></footer>;
 };
