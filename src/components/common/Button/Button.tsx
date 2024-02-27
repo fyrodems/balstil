1b2db0ed8f52703dc15b1be19738a06542d1818f
martyna <fyrodems@gmail.com>
2024-02-27 10:32:25 +0100

    delete all imgs v2

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,14 @@
-"use client";
+'use client';
 
 // import { Link } from '@/navigation';
-import classNames from "classnames";
-import { useLocale } from "next-intl";
-import Link from "next/link";
-import { FC, useState } from "react";
+import classNames from 'classnames';
+import { useLocale } from 'next-intl';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC, useState } from 'react';
 
 interface ButtonProps {
-  type?: "primary" | "secondary" | "tertiary";
+  type?: 'primary' | 'secondary' | 'tertiary';
   content?: string | React.ReactNode;
   children?: React.ReactNode;
   showArrow?: boolean;
@@ -16,14 +17,14 @@ interface ButtonProps {
 }
 
 export const Button: FC<ButtonProps> = ({
-  type = "primary",
+  type = 'primary',
   content,
   children,
   showArrow = true,
   className,
   to,
 }) => {
-  const locale = useLocale() as "en" | "pl" | undefined;
+  const locale = useLocale() as 'en' | 'pl' | undefined;
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -31,11 +32,11 @@ export const Button: FC<ButtonProps> = ({
       <button
         className={classNames(
           className,
-          type === "primary"
-            ? "bg-main-color text-white"
-            : "bg-button-bg text-black",
-          "font-medium flex center rounded-[50px] p-2 px-5",
-          "relative overflow-hidden transition-transform duration-300"
+          type === 'primary'
+            ? 'bg-main-color text-white'
+            : 'bg-button-bg text-black',
+          'font-medium flex center rounded-[50px] p-2 px-5',
+          'relative overflow-hidden transition-transform duration-300'
         )}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
@@ -45,18 +46,20 @@ export const Button: FC<ButtonProps> = ({
           {children}
           {showArrow && (
             <div className="w-7 h-7 relative ml-2">
-              <img
+              <Image
+                width={31}
+                height={31}
                 src={
-                  type === "primary"
-                    ? "/assets/common/buttonArrow.svg"
-                    : type === "secondary"
-                    ? "/assets/common/buttonArrowBlack.svg"
-                    : "/assets/common/buttonArrowGold.svg"
+                  type === 'primary'
+                    ? '/assets/common/buttonArrow.svg'
+                    : type === 'secondary'
+                    ? '/assets/common/buttonArrowBlack.svg'
+                    : '/assets/common/buttonArrowGold.svg'
                 }
                 alt=""
                 className={classNames(
-                  "w-full h-full absolute transform transition-transform duration-300 ",
-                  { "translate-x-[4px] translate-y-[-4px]": isHovered }
+                  'w-full h-full absolute transform transition-transform duration-300 ',
+                  { 'translate-x-[4px] translate-y-[-4px]': isHovered }
                 )}
               />
             </div>
